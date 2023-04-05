4f29158e470f6c9fde8a3e56cebeecadbe6a5c45
unknown <o.k.elaref@gmail.com>
2023-04-05 19:36:25 -0400

    making the UML diagrams

--- pathfinder/src/test/java/ca/mcmaster/pathfinder/DijkstraTest.java
+++ pathfinder/src/test/java/ca/mcmaster/pathfinder/DijkstraTest.java
@@ -1,20 +1,14 @@
 package ca.mcmaster.pathfinder;
 
-import org.junit.jupiter.api.Test;
 
-import ca.mcmaster.pathfinder.Attribute;
-import ca.mcmaster.pathfinder.DijkstraPathFinder;
-import ca.mcmaster.pathfinder.Edge;
-import ca.mcmaster.pathfinder.Graph;
-import ca.mcmaster.pathfinder.Node;
-import ca.mcmaster.pathfinder.PathFinder;
-import ca.mcmaster.pathfinder.SimpleAttribute;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-public class DijkstraTesting {
+public class DijkstraTest {
     
     @Test
     public void shortestPathTest(){
@@ -48,6 +42,37 @@ public class DijkstraTesting {
 
     }
 
+    @Test
+    public void shortestPathTest2(){
+
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+
+        Edge edge1_2 = new Edge(1, node1, node2);
+        Edge edge1_3 = new Edge(2, node1, node3);
+        Edge edge2_3 = new Edge(3, node2, node3);
+
+        edge1_2.addAttribute(new SimpleAttribute("weight", 10));
+        edge1_3.addAttribute(new SimpleAttribute("weight", 10));
+        edge2_3.addAttribute(new SimpleAttribute("weight", 5));
+
+
+        Graph graph = new Graph();
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addNode(node3);
+
+        graph.addEdge(edge1_2);
+        graph.addEdge(edge1_3);
+        graph.addEdge(edge2_3);
+
+        PathFinder shortest = new DijkstraPathFinder();
+        shortest.findPath(graph, 1, 2);
+        int cost = pathCost(shortest.findPath(graph, 1, 2));
+        assertEquals(cost, 10);
+    }
+
     private int pathCost(List<Edge> edges){
         int totalWeight = 0;
 
@@ -55,5 +80,5 @@ public class DijkstraTesting {
             totalWeight += (int) e.getAttribute("weight");
         }
         return totalWeight;
-    }
+    }  
 }
